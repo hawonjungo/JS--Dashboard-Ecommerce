c44f169e4cefff245282ddbf7b8634edfa6b4c2d
hawonjungo <hawon.jungo@gmail.com>
2022-05-27 22:17:07 +1000

    Sidebar, navbar and menu responsive done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,13 @@ import { links} from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-    const {activeMenu, setActiveMenu} = useStateContext();
+    const {activeMenu, setActiveMenu,screenSize} = useStateContext();
 
+    const handleCloseSideBar = ()=>{
+      if(activeMenu && screenSize <=900){
+        setActiveMenu(false);
+      }
+    }
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pd-2.5 rounded-lg text-white text-md m-2';
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pd-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
@@ -17,7 +22,7 @@ const Sidebar = () => {
     overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (<>
         <div className="flex justify-between items-center">
-          <Link to="/" onClick={() => setActiveMenu(false)}
+          <Link to="/" onClick={() => handleCloseSideBar}
            className="items-center gap-3 mt-4 
           flex text-xl font-extrabold
           tracking-tight dark:text-white text-slate-900">
@@ -41,7 +46,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({isActive})=>
                     isActive ? activeLink : normalLink}>
                       {link.icon}
