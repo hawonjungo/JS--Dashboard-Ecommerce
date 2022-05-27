094e9992ddadc097cdc83bb8ae02aedda9683469
hawonjungo <hawon.jungo@gmail.com>
2022-05-28 03:24:11 +1000

    Main page with SparkLine char Error

--- src/App.js
+++ src/App.js
@@ -44,9 +44,8 @@ const App = () => {
             <div className="fixed md:static bg-main-bg 
             dark:bg-main-dark-bg navbar w-full">
               <Navbar />
-            </div>
-            </div>
-            <div>
+            </div>            
+           <div>
 
               <Routes>
               
@@ -74,7 +73,8 @@ const App = () => {
                 <Route path="/pyramid" element={<Pyramid />} />
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
-            </div>          
+            </div> 
+          </div>         
         </div>
       </BrowserRouter>
     </div>
